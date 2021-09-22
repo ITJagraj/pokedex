ee83da0b8c173a20936804c1e59107d01ffe6d61
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-09-22 13:07:38 -0400

    pokemon displays

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const pokedex = document.getElementById('pokedex');
-const promises = [];
-const fetchPokemon = () => {
-    for(let i = 1; i<= 150; i++); {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promise.push(fetch(url)
-    .then((res) => res.json()));
-    }
-    promise.all(promises).then(results => {
-        const pokemon = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            type: data.types
-            .map((type) => type.type.name)
-            .join(', ')
 
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
         }));
-    });   
+        displayPokemon(pokemon);
+    });
 };
 
-const displayPokemon = (pokemon => {
-    const html = `<li>Bulbasaur</li>`;
-    pokedex.innerHTML = html;
-})
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHtmalString = pokemon.map ( pokemon => `
+    <li>
+    <img src = "${pokemon.image}"
+    <h2>${pokemon.id}. ${pokemon.name}</h2>
+    <p>Type: ${pokemon.type}</p>
+    </li>
+    `)
+    .join('')
+    pokedex.innerHTML = pokemonHtmalString;
+}
 
 fetchPokemon();
