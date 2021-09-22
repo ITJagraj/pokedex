bf3f58430432669e9b65508712b8a63bfe9e8ec6
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-09-22 12:07:02 -0400

    promise

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const promises = [];
 const fetchPokemon = () => {
-    for(let i = 1; i <= 150; i++ );
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
-
-    
-    promises.all(promises).then(data => {
-        const pokemon = results.map((data) => ({
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+fetch(url)
+    .then(res => {
+         return res.json()
+    })
+    .then(data => {
+        console.log(data);
+        const pokemon = {
             name: data.name,
             id: data.id,
-            image: data.sprits['front_default'],
-            type: data.types.map(type => type.type.name).join(', ')
-        })); 
+            image: data.sprites['front_default'],
+            type: data.types
+            .map((type) => type.type.name)
+            .join(', ')
+        };
         console.log(pokemon);
     })
-}
+         
+};
+
 fetchPokemon();
