494fc1020793d03adc000abc4ae0e64a22019720
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-09-22 12:31:19 -0400

    generating HTML

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+const pokedex = document.getElementById('pokedex');
+const promises = [];
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-fetch(url)
-    .then(res => {
-         return res.json()
-    })
-    .then(data => {
-        console.log(data);
-        const pokemon = {
+    for(let i = 1; i<= 150; i++); {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promise.push(fetch(url)
+    .then((res) => res.json()));
+    }
+    promise.all(promises).then(results => {
+        const pokemon = results.map((data) => ({
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
             type: data.types
             .map((type) => type.type.name)
             .join(', ')
-        };
-        console.log(pokemon);
-    })
-         
+
+        }));
+    });   
 };
+
+const displayPokemon = (pokemon => {
+    const html = `<li>Bulbasaur</li>`;
+    pokedex.innerHTML = html;
+})
 
 fetchPokemon();
